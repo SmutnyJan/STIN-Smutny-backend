a5d7382538ef45740011ea1bbd9b573a1a1c9c57
SmutnyJan <smutnyjan2003@seznam.cz>
2024-05-08 22:07:50 +0200

    initial commit

---
+++ STINWebApiSmutny/STINWebApiSmutny/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace STINWebApiSmutny.Models
+{
+    public class User
+    {
+        public Int32 id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
