d93f9e2c364f744b3d2515859256d07526fb5b4c
SmutnyJan <smutnyjan2003@seznam.cz>
2024-05-14 03:37:09 +0200

    new endpoints

---
+++ STINWebApiSmutny/STINWebApiSmutny/Models/UserRegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace STINWebApiSmutny.Models
+{
+    public class UserRegisterModel
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
